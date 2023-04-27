b613f887f1aebdb562802091f215306b53234ad9
matvis01 <mati.siwy@op.pl>
2023-04-28 00:31:23 +0200

    morbin

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,36 @@ import NavBar from "../components/navBar"
 import AddPlace from "../components/addPlace"
 import EventCard from "../components/eventCard"
 import FilterBar from "../components/filterBar"
-import { Button } from "@mui/material"
+import { Button, Container } from "@mui/material"
 
 export default function Home() {
   const [addPlace, setAddPlace] = useState(false)
   const handleOpen = () => setAddPlace(true)
   const handleClose = () => setAddPlace(false)
-//test commit 
+  //test commit
   return (
     <>
       <NavBar />
       <FilterBar />
-      <div className="addCard">
-      <Button className="addButton" onClick={handleOpen}>add place</Button>
-      </div>
-      <AddPlace open={addPlace} handleClose={handleClose} />
-      <EventCard />
-      <EventCard />
-      <EventCard />
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginBottom: "100px",
+          width: "100%",
+        }}
+      >
+        <div className="addCard">
+          <Button className="addButton" onClick={handleOpen}>
+            add place
+          </Button>
+        </div>
+        <AddPlace open={addPlace} handleClose={handleClose} />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+      </Container>
     </>
   )
 }
