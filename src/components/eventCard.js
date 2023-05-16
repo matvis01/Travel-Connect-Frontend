4c73d53aed776539761aa21811fe86dae4d9bb1e
matvis01 <mati.siwy@op.pl>
2023-05-16 02:29:03 +0200

    wyswietlanie event cardow

---
+++ src/components/eventCard.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react"
+import Card from "@mui/material/Card"
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import api from "../api/api"
+export default function eventCard(props) {
+  const {
+    id,
+    name,
+    description,
+    startsAt,
+    endsAt,
+    canSignUp,
+    isSignedUp,
+    isEditable,
+  } = props.event
+  const [isSignedUpState, setIsSignedUpState] = useState(isSignedUp)
+
+  async function handleSignUpOrOut() {
+    const upOrOut = isSignedUpState ? "sign-out" : "sign-up"
+    try {
+      const res = await api.put(
+        `/Events/${id}/${upOrOut}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+      setIsSignedUpState(!isSignedUpState)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      {/* <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+      /> */}
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+        <Typography> starts at: {startsAt}</Typography>
+        <Typography> ends at: {endsAt}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">See Details</Button>
+        {canSignUp && (
+          <Button size="small" onClick={handleSignUpOrOut}>
+            {isSignedUpState ? "sign out" : "sign up"}
+          </Button>
+        )}
+      </CardActions>
+    </Card>
+  )
+}
--- src/pages/events.js
+++ src/pages/events.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react"
 import NavBar from "../components/navBar"
 import FilterBar from "../components/filterBar"
 import api from "../api/api"
-
+import EventCard from "@/components/eventCard"
+import { Container } from "@mui/material"
 export default function Events() {
   const [events, setEvents] = useState([])
   const [categories, setCategories] = useState()
@@ -35,7 +36,6 @@ export default function Events() {
         },
       })
       setEvents(res.data)
-      console.log(res.data)
     } catch (err) {
       console.log(err)
     }
@@ -44,13 +44,27 @@ export default function Events() {
   return (
     <>
       <NavBar />
-      <FilterBar
+      {/* <FilterBar
         categories={categories}
         changePlaces={(elements) => setPlaces(elements)}
         applyFilters={(c, t) => {
           // fetchEvents(c, t)
         }}
-      />
+      /> */}
+      <Container
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          margin: "10px",
+          padding: "10px",
+          gap: "10px",
+        }}
+      >
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </Container>
     </>
   )
 }
